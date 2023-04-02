bc79ba3303d28830b11c0121881fd7094ccec340
frankmicrobit <100852447+frankmicrobit@users.noreply.github.com>
2023-04-02 19:53:16 +0200

    Safety function for lid up. Stops anyway, after 1.6 seconds.  Better calibration

--- main.ts
+++ main.ts
@@ -2,10 +2,11 @@ input.onButtonPressed(Button.A, function () {
     doCalibrate()
 })
 input.onButtonPressed(Button.B, function () {
-    pins.digitalWritePin(DigitalPin.P0, 1)
+    IsStoped = !(IsStoped)
 })
 function doCalibrate () {
-    IsCalibrating = true
+    IsStoped = true
+    pins.digitalWritePin(DigitalPin.P8, 1)
     pins.digitalWritePin(DigitalPin.P0, 0)
     basic.showIcon(IconNames.Heart)
     basic.pause(1000)
@@ -16,9 +17,13 @@ function doCalibrate () {
         . # . . .
         . . # . .
         `)
-    servos.P2.run(-20)
+    StartTime = input.runningTime()
+    servos.P2.run(-35)
     while (pins.digitalReadPin(DigitalPin.P0) == 0) {
         basic.pause(10)
+        if (input.runningTime() > StartTime + 700) {
+            servos.P2.run(-20)
+        }
     }
     servos.P2.stop()
     basic.showLeds(`
@@ -40,18 +45,25 @@ function doCalibrate () {
     basic.pause(600)
     servos.P2.stop()
     basic.clearScreen()
+    pins.digitalWritePin(DigitalPin.P8, 0)
     IsOpen = false
-    IsCalibrating = false
+    IsStoped = false
 }
 function doOpenLid () {
     if (!(IsOpen)) {
+        StartTime = input.runningTime()
+        pins.digitalWritePin(DigitalPin.P8, 1)
         servos.P2.run(-80)
         basic.pause(400)
         servos.P2.run(-50)
         basic.pause(100)
         servos.P2.run(-20)
-        while (pins.digitalReadPin(DigitalPin.P0) == 0) {
+        while (pins.digitalReadPin(DigitalPin.P0) == 0 && !(IsStoped)) {
             basic.pause(10)
+            if (input.runningTime() > StartTime + 1550) {
+                IsStoped = true
+                basic.showIcon(IconNames.No)
+            }
         }
         servos.P2.stop()
         IsOpen = true
@@ -62,17 +74,19 @@ function doCloseLid () {
         servos.P2.run(50)
         basic.pause(600)
         servos.P2.stop()
+        pins.digitalWritePin(DigitalPin.P8, 0)
         IsOpen = false
     }
 }
 let Counter = 0
+let StartTime = 0
 let IsOpen = false
-let IsCalibrating = false
+let IsStoped = false
 basic.showNumber(0)
 servos.P2.setStopOnNeutral(false)
 servos.P2.stop()
 basic.showNumber(1)
-IsCalibrating = true
+IsStoped = true
 radio.setGroup(1)
 let UpTime = 1000
 let LoopCount = 0
@@ -81,7 +95,7 @@ IsOpen = false
 basic.showNumber(2)
 doCalibrate()
 basic.forever(function () {
-    if (!(IsCalibrating)) {
+    if (!(IsStoped)) {
         Distance = Environment.sonarbit_distance(Environment.Distance_Unit.Distance_Unit_cm, DigitalPin.P1)
         if (Distance > 30) {
             if (!(IsOpen)) {
@@ -109,6 +123,6 @@ basic.forever(function () {
                 doCloseLid()
             }
         }
-        basic.pause(100)
     }
+    basic.pause(100)
 })
