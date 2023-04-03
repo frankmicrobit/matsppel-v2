ef6e39edcdc7d77a98ddea2019ecb0ff203f5182
frankmicrobit <100852447+frankmicrobit@users.noreply.github.com>
2023-04-03 16:02:41 +0200

    Signaling status to ch 1

--- main.ts
+++ main.ts
@@ -8,6 +8,7 @@ function doCalibrate () {
     IsStoped = true
     pins.digitalWritePin(DigitalPin.P8, 1)
     pins.digitalWritePin(DigitalPin.P0, 0)
+    radio.sendValue("C", 0)
     basic.showIcon(IconNames.Heart)
     basic.pause(1000)
     basic.showLeds(`
@@ -26,13 +27,6 @@ function doCalibrate () {
         }
     }
     servos.P2.stop()
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        # . # . #
-        . . . . .
-        . . . . .
-        `)
     basic.pause(1000)
     basic.showLeds(`
         . . # . .
@@ -46,6 +40,7 @@ function doCalibrate () {
     servos.P2.stop()
     basic.clearScreen()
     pins.digitalWritePin(DigitalPin.P8, 0)
+    radio.sendValue("C", 1)
     IsOpen = false
     IsStoped = false
 }
@@ -60,12 +55,15 @@ function doOpenLid () {
         servos.P2.run(-20)
         while (pins.digitalReadPin(DigitalPin.P0) == 0 && !(IsStoped)) {
             basic.pause(10)
-            if (input.runningTime() > StartTime + 1550) {
+            if (input.runningTime() > StartTime + 1600) {
                 IsStoped = true
                 basic.showIcon(IconNames.No)
             }
         }
         servos.P2.stop()
+        if (IsStoped) {
+            radio.sendValue("S", Counter)
+        }
         IsOpen = true
     }
 }
@@ -82,22 +80,56 @@ let Counter = 0
 let StartTime = 0
 let IsOpen = false
 let IsStoped = false
+enum Distance_Unit {
+    Distance_Unit_mm,
+    Distance_Unit_cm,
+    Distance_Unit_inch,
+}
+function sonarbit_distance(distance_unit: Distance_Unit, pin: DigitalPin): number {
+
+    // send pulse
+    pins.setPull(pin, PinPullMode.PullNone)
+    pins.digitalWritePin(pin, 0)
+    control.waitMicros(2)
+    pins.digitalWritePin(pin, 1)
+    control.waitMicros(10)
+    pins.digitalWritePin(pin, 0)
+
+    // read pulse
+    let d = pins.pulseIn(pin, PulseValue.High, 23000)  // 8 / 340 = 
+    let distance = d * 10 * 5 / 3 / 58
+
+    if (distance > 4000) distance = 0
+
+    switch (distance_unit) {
+        case 0:
+            return Math.round(distance) //mm
+            break
+        case 1:
+            return Math.round(distance / 10)  //cm
+            break
+        case 2:
+            return Math.round(distance / 25)  //inch
+            break
+        default:
+            return 0
+
+    }
+}
+let Distance = sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P1)
 basic.showNumber(0)
 servos.P2.setStopOnNeutral(false)
 servos.P2.stop()
 basic.showNumber(1)
 IsStoped = true
 radio.setGroup(1)
-let UpTime = 1000
-let LoopCount = 0
-let Distance = Environment.sonarbit_distance(Environment.Distance_Unit.Distance_Unit_cm, DigitalPin.P1)
 IsOpen = false
 basic.showNumber(2)
 doCalibrate()
 basic.forever(function () {
     if (!(IsStoped)) {
-        Distance = Environment.sonarbit_distance(Environment.Distance_Unit.Distance_Unit_cm, DigitalPin.P1)
-        if (Distance > 30) {
+        Distance = sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P1)
+if (Distance > 30) {
             if (!(IsOpen)) {
                 basic.showLeds(`
                     . . # . .
@@ -107,11 +139,11 @@ basic.forever(function () {
                     . . # . .
                     `)
                 doOpenLid()
+                radio.sendValue("U", Counter)
             }
         } else {
             if (IsOpen) {
                 Counter += 1
-                radio.sendNumber(Counter)
                 basic.showLeds(`
                     . . # . .
                     . . . # .
@@ -121,6 +153,7 @@ basic.forever(function () {
                     `)
                 basic.pause(3000)
                 doCloseLid()
+                radio.sendValue("D", Counter)
             }
         }
     }
